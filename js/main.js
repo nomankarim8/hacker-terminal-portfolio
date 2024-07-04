808044036cd65f09e9cd7bfe8c3708d6fde227fe
nomankarim8 <karimnoman97@gmail.com>
2024-07-04 00:43:35 -0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -33,6 +33,8 @@ const projectContent = document.querySelector('#project-content')
 
 
 
+
+
 about.addEventListener('click', () => {  //have to change this one 
   const aboutBox = new WinBox({  //have to change this one 
     title: 'About Me',
