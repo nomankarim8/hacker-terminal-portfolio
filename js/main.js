b4d32564b02f93821f84ecd5d94e52ccb30dc0a9
nomankarim8 <karimnoman97@gmail.com>
2024-07-04 00:44:53 -0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -33,8 +33,6 @@ about.addEventListener('click', () => {  //have to change this one
 
 
 
-
-
 contact.addEventListener('click', () => {  //have to change this one 
   const contactBox = new WinBox({  //have to change this one 
     title: 'contact Me',
