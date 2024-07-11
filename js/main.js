3c33703df5e5bd14225c9e912aab9999ebd1539b
nomankarim8 <karimnoman97@gmail.com>
2024-07-11 02:23:00 -0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ project.addEventListener('click', () => {
     right: 50,
     bottom: 50,
     left: 50,
-    mount: projectContent, 
+    mount: projectContent, //have to change this one --whomi8 
     onfocus: function () {
       this.setBackground('#00aa00')
     },
