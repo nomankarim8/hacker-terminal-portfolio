358552921f42bd21f45706fb285ae143f6935246
nomankarim8 <karimnoman97@gmail.com>
2024-12-18 20:33:02 -0800

    update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
 const project = document.querySelector('#project')
 
+
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 const projectContent = document.querySelector('#project-content')
